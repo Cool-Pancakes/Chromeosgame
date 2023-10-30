dfbdcf8f312aa1b3f8330bdd275fdafa95038565
Cool-Pancakes <110803123+Cool-Pancakes@users.noreply.github.com>
2023-10-30 16:54:42 +1300

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,4 +1,5 @@
 sudo apt install x11-apps
-xeyes
-xlogo
-xmessage this is a little test
+xmessage this is a fake virus, do not panic
+xmessage your computer has got the gmae virus, would you like to diagnose it?
+xmessage we could not diagnose your computer. Powerwash has begun
+xmessage type ./Chromeosgame/main.sh to run this again
